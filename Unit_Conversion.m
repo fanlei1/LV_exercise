bded40ea574323f1b7f18f491b95b4540f85fa64
fanlei1 <86135321+fanlei1@users.noreply.github.com>
2022-09-18 19:48:50 -0400

    initial

---
+++ Unit_Conversion.m
@@ -0,0 +1,10 @@
+% Unit conversions:
+% From: http://www.onlineconversion.com/
+um2m = 1e-6;  % micron to meter
+um2mm = 1e-3; % micron to millimeter
+mmHg2Pa = 133.32239; % pressure in mmHg to Pascals
+mmHg2MPa = 0.00013332239;% pressure in mmHg to MegaPascals
+Pa2MPa = 1e-6; % pressure from Pascal to MegaPascal
+dynpcm22MPa =  1.0e-7; % pressure in dynes/cm2 to MegaPascals
+dynpcm22Pa = 0.1;
+mm3ps2mLpmin = 6e-2; % flow in millimeter3/second to milliliter/minute
